8194190dfb4530d3dee6bc4f1753c43fafc12032
Anatoly <tol.semenoff>
2023-07-23 23:53:30 +0400

    Обновил Enter
    Signed-off-by: Anatoly <tol.semenoff>

--- elevator.cpp
+++ elevator.cpp
@@ -16,16 +16,16 @@ Elevator::~Elevator(){
 void Elevator::defaultValues() {
 	lowerFloor = 1;
 	upperFloor = 10;
-	outPower = 1;
-	inStatus = 1;
+	outPower = true;
+	inStatus = true;
 	floorNow = 5;
 }
 
 void Elevator::howСurrentStatus(){
-	std::cout << "Нижний этаж: " << lowerFloor;
-	std::cout << "Верхний этаж: " << upperFloor;
-	std::cout << "Питание лифта: " << outPower;
-	std::cout << "Состояние лифта: " << inStatus;
+	std::cout << "Нижний этаж: " << lowerFloor << std::endl;
+	std::cout << "Верхний этаж: " << upperFloor << std::endl;
+	std::cout << "Питание лифта: " << outPower << std::endl;
+	std::cout << "Состояние лифта: " << inStatus << std::endl;
 	std::cout << "На какой этаж нужно переместиться: " << floorNow;
 
 
